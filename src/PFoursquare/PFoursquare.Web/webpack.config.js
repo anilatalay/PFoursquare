6d722d88b8246e48c21d985b614223f702e25abd
Anıl Atalay <anilatalay@hotmail.com.tr>
2017-10-22 14:40:25 +0300

    Cleared Angular Project

--- src/PFoursquare/PFoursquare.Web/webpack.config.js
+++ src/PFoursquare/PFoursquare.Web/webpack.config.js
@@ -1,85 +1,84 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
-const treeShakableModules = [
-    '@angular/animations',
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/core',
-    '@angular/forms',
-    '@angular/http',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/router',
-    'zone.js'
-];
-const nonTreeShakableModules = [
-    'es6-promise',
-    'es6-shim',
-    'event-source-polyfill'
-];
-const allModules = treeShakableModules.concat(nonTreeShakableModules);
+const AotPlugin = require('@ngtools/webpack').AotPlugin;
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
 module.exports = (env) => {
-    const extractCSS = new ExtractTextPlugin('vendor.css');
+    // Configuration in common to both client-side and server-side bundles
     const isDevBuild = !(env && env.prod);
     const sharedConfig = {
-        stats: {
-            modules: false
-        },
-        resolve: {
-            extensions: ['.js']
+        stats: { modules: false },
+        context: __dirname,
+        resolve: { extensions: ['.js', '.ts'] },
+        output: {
+            filename: '[name].js',
+            publicPath: 'dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
         },
         module: {
-            rules: [{
-                test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/,
-                use: 'url-loader?limit=100000'
-            }]
+            rules: [
+                { test: /\.ts$/, include: /ClientApp/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] : '@ngtools/webpack' },
+                { test: /\.html$/, use: 'html-loader?minimize=false' },
+                { test: /\.css$/, use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize'] },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+            ]
         },
-        output: {
-            publicPath: 'dist/',
-            filename: '[name].js',
-            library: '[name]_[hash]'
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery'
-            }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
-            new webpack.ContextReplacementPlugin(/\@angular\b.*\b(bundles|linker)/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/11580
-            new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/14898
-            new webpack.IgnorePlugin(/^vertx$/) // Workaround for https://github.com/stefanpenner/es6-promise/issues/100
-        ]
+        plugins: [new CheckerPlugin()]
     };
 
+    // Configuration for client-side bundle suitable for running in browsers
+    const clientBundleOutputDir = './wwwroot/dist';
     const clientBundleConfig = merge(sharedConfig, {
-        entry: {
-            // To keep development builds fast, include all vendor dependencies in the vendor bundle.
-            // But for production builds, leave the tree-shakable ones out so the AOT compiler can produce a smaller bundle.
-            vendor: isDevBuild ? allModules : nonTreeShakableModules
-        },
-        output: {
-            path: path.join(__dirname, 'wwwroot', 'dist')
-        },
-        module: {
-            rules: [{
-                test: /\.css(\?|$)/,
-                use: extractCSS.extract({
-                    use: isDevBuild ? 'css-loader' : 'css-loader?minimize'
-                })
-            }]
-        },
+        entry: { 'main-client': './ClientApp/boot.browser.ts' },
+        output: { path: path.join(__dirname, clientBundleOutputDir) },
         plugins: [
-            extractCSS,
-            new webpack.DllPlugin({
-                path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-                name: '[name]_[hash]'
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/dist/vendor-manifest.json')
             })
-        ].concat(isDevBuild ? [] : [
-            new webpack.optimize.UglifyJsPlugin()
+        ].concat(isDevBuild ? [
+            // Plugins that apply in development builds only
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ] : [
+            // Plugins that apply in production builds only
+            new webpack.optimize.UglifyJsPlugin(),
+            new AotPlugin({
+                tsConfigPath: './tsconfig.json',
+                entryModule: path.join(__dirname, 'ClientApp/app/app.module.browser#AppModule'),
+                exclude: ['./**/*.server.ts']
+            })
         ])
     });
 
-    return [clientBundleConfig];
-}
+    // Configuration for server-side (prerendering) bundle suitable for running in Node
+    const serverBundleConfig = merge(sharedConfig, {
+        resolve: { mainFields: ['main'] },
+        entry: { 'main-server': './ClientApp/boot.server.ts' },
+        plugins: [
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./ClientApp/dist/vendor-manifest.json'),
+                sourceType: 'commonjs2',
+                name: './vendor'
+            })
+        ].concat(isDevBuild ? [] : [
+            // Plugins that apply in production builds only
+            new AotPlugin({
+                tsConfigPath: './tsconfig.json',
+                entryModule: path.join(__dirname, 'ClientApp/app/app.module.server#AppModule'),
+                exclude: ['./**/*.browser.ts']
+            })
+        ]),
+        output: {
+            libraryTarget: 'commonjs',
+            path: path.join(__dirname, './ClientApp/dist')
+        },
+        target: 'node',
+        devtool: 'inline-source-map'
+    });
+
+    return [clientBundleConfig, serverBundleConfig];
+};
